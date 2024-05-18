d3cfaa0b837939b579206a7260f817827cefd589
NikolaiCharaev <hayyabusa_01@mail.ru>
2024-05-18 18:37:23 +0000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <>
-  
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nam necessitatibus? Veniam dicta pariatur aspernatur nobis nisi numquam quas fugiat, ipsum illum vel excepturi, architecto similique mollitia? Qui, maiores quam!
     <Button>click me beach</Button>
   </>
   );
