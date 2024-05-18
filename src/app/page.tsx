eecf86cdbe24134f59784991e47d845dd5d214e5
NikolaiCharaev <hayyabusa_01@mail.ru>
2024-05-18 19:38:06 +0000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ export default function Home() {
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nam necessitatibus? Veniam dicta pariatur aspernatur nobis nisi numquam quas fugiat, ipsum illum vel excepturi, architecto similique mollitia? Qui, maiores quam!
 
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nam magnam harum quasi perferendis. Architecto iusto sequi dolores dignissimos ea tempora velit eligendi possimus! Dolorem quos impedit atque cupiditate animi!
+
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veniam facilis magnam perferendis ut expedita, quaerat rerum mollitia, ea tempora saepe cupiditate illum placeat. Eaque ut quibusdam quis explicabo sunt?
     <Button>click me beach</Button>
   </>
   );
